afde2c0c4890e02749fd464945dcfcdc44491c44
Arina Venskikh <tisha201@yandex.ru>
2021-12-21 21:55:17 +0400

    Sprint_4

--- src/main/java/ru/yandex/praktikum/Account.java
+++ src/main/java/ru/yandex/praktikum/Account.java
@@ -13,10 +13,6 @@ public class Account {
         for(int i = 0; i < name.length(); i++) {
             if(Character.isWhitespace(name.charAt(i))) count++;
         }
-        /*
-             Этот метод должен проверять, что сохранённая через конструктор строка соответствует требованиям.
-             Если строка удовлетворяет условиям, метод возвращает true, иначе — false.
-         */
-        return name.length() >= 3 & name.length() <= 19 & count == 1 & !name.startsWith(" ") & !name.endsWith(" ");
+        return name.length() >= 3 & name.length() <= 19 & !name.startsWith(" ") & !name.endsWith(" ") & count == 1;
     }
 }
--- src/main/java/ru/yandex/praktikum/Praktikum.java
+++ src/main/java/ru/yandex/praktikum/Praktikum.java
@@ -2,11 +2,6 @@ package ru.yandex.praktikum;
 
 public class Praktikum {
     public static void main(String[] args) {
-        /*
-             В этом методе заложи логику работы с классом Account.
-             Нужно создать экземпляр класса Account: в качестве аргумента передать тестируемое имя
-             и вызвать метод, который проверяет, можно ли использовать фамилию и имя для печати на банковской карте.
-         */
         Account account = new Account("Тимоти Шалаламе");
         System.out.println(account.checkNameToEmboss());
     }
